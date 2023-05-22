429dd7deb3c0c323e1e7004ffeb2a3371c544093
Maslova95 <maslovaivit@gmail.com>
2023-05-22 15:22:27 +0300

    new homework

--- index.js
+++ index.js
@@ -1,77 +1,77 @@
-// Завдання 2
+// // Завдання 2
 
-    alert("Maslova")
+//     alert("Maslova")
 
-// завдання 3
+// // завдання 3
 
-    //   a) оголосіть дві змінні;
-    //   b) запишіть у змінні будь-які значення;
-    let a = 5;
-    let b = 12;
-    //   c) виведіть на екран значення змінних;
-    console.log(a,b)
-    //   d) скопіюйте значення однієї змінної в іншу;
-    b = a;
-    //   e) виведіть на екран значення змінних.
-    console.log(a,b)
+//     //   a) оголосіть дві змінні;
+//     //   b) запишіть у змінні будь-які значення;
+//     let a = 5;
+//     let b = 12;
+//     //   c) виведіть на екран значення змінних;
+//     console.log(a,b)
+//     //   d) скопіюйте значення однієї змінної в іншу;
+//     b = a;
+//     //   e) виведіть на екран значення змінних.
+//     console.log(a,b)
 
-// завдання 4
+// // завдання 4
 
-let student = {
-        name: "Dmytro",
-        age: 38,
-        info: null,
-        additionally: undefined,
-        sample: true
-    };
+// let student = {
+//         name: "Dmytro",
+//         age: 38,
+//         info: null,
+//         additionally: undefined,
+//         sample: true
+//     };
 
-    console.log(student)
+//     console.log(student)
 
-// завдання 5
+// // завдання 5
 
-    const age = prompt("Have you reached the age of majority?");
-    const isAdult = age;
-    console.log(age)
+//     const age = prompt("Have you reached the age of majority?");
+//     const isAdult = age;
+//     console.log(age)
 
-// завдання 6
+// // завдання 6
 
-    const firstName = "Iryna";
-    const lastName = "Maslova";
-    const group = "U-1";
-    const birthYear = 1995;
+//     const firstName = "Iryna";
+//     const lastName = "Maslova";
+//     const group = "U-1";
+//     const birthYear = 1995;
 
-    const isMarried = false;
+//     const isMarried = false;
 
-    console.log(typeof firstName);  // string
-    console.log(typeof isMarried);  // boolean
-    console.log(typeof group);      // string
-    console.log(typeof birthYear);  // number
+//     console.log(typeof firstName);  // string
+//     console.log(typeof isMarried);  // boolean
+//     console.log(typeof group);      // string
+//     console.log(typeof birthYear);  // number
 
-    const nullVariable = null;
-    let undefinedVariable;
+//     const nullVariable = null;
+//     let undefinedVariable;
 
-    console.log(typeof nullVariable);      // object
-    console.log(typeof undefinedVariable); // undefined
+//     console.log(typeof nullVariable);      // object
+//     console.log(typeof undefinedVariable); // undefined
 
-// завдання 7
+// // завдання 7
 
-    const login = prompt("Введіть ваш логін:");
-    const email = prompt("Введіть вашу електронну пошту:");
-    const password = prompt("Введіть ваш пароль:");
+//     const login = prompt("Введіть ваш логін:");
+//     const email = prompt("Введіть вашу електронну пошту:");
+//     const password = prompt("Введіть ваш пароль:");
 
-    const message = "Dear User, your email is " + email + ", your password is " + password;
+//     const message = "Dear User, your email is " + email + ", your password is " + password;
 
-    console.log(message);
+//     console.log(message);
 
-// завдання 8
+// // завдання 8
 
-    const secondsInHour = 60 * 60;
-    const secondsInDay = secondsInHour * 24;
-    const secondsInMonth = secondsInDay * 30;  // Припустимо, що місяць складається з 30 днів
+//     const secondsInHour = 60 * 60;
+//     const secondsInDay = secondsInHour * 24;
+//     const secondsInMonth = secondsInDay * 30;  // Припустимо, що місяць складається з 30 днів
 
-    console.log("Кількість секунд в годині: " + secondsInHour);
-    console.log("Кількість секунд в добі: " + secondsInDay);
-    console.log("Кількість секунд в місяці: " + secondsInMonth);
+//     console.log("Кількість секунд в годині: " + secondsInHour);
+//     console.log("Кількість секунд в добі: " + secondsInDay);
+//     console.log("Кількість секунд в місяці: " + secondsInMonth);
 
 
 
