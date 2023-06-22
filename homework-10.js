1f2cf8f352cbf10ce9c85a7cfb13b23b06df20a8
Maslova95 <maslovaivit@gmail.com>
2023-06-22 21:53:50 +0300

    sample code

--- homework-10.js
+++ homework-10.js
@@ -25,4 +25,91 @@ let data = {
   console.log(age2); // 24
   console.log(name4); // "Bob"
   console.log(age4); // 26
+
+  // 3
+
+  function mul(...args) {
+    let result = 1;
+    let hasNumber = false;
+  
+    for (const arg of args) {
+      if (typeof arg === 'number') {
+        result *= arg;
+        hasNumber = true;
+      }
+    }
+  
+    if (hasNumber) {
+      return result;
+    } else {
+      return 0;
+    }
+  }
+  
+  console.log(mul(1, "str", 2, 3, true)); // 6
+  console.log(mul(null, "str", false, true)); // 0
+
+  // 4
+
+  let server = {
+    data: 0,
+    convertToString: function (callback) {
+      callback(() => {
+        return this.data + "";
+      });
+    }
+  };
+  
+  let client = {
+    server: server,
+    result: "",
+    calc: function (data) {
+      this.server.data = data;
+      this.server.convertToString(this.notification());
+    },
+    notification: function () {
+      return (() => {
+        this.result = callback();
+      });
+    }
+  };
+  
+  client.calc(123);
+  console.log(client.result); // "123"
+  console.log(typeof client.result); // "string"
+
+  // 5
+
+  function mapBuilder(keysArray, valuesArray) {
+    const map = new Map();
+  
+    for (let i = 0; i < keysArray.length; i++) {
+      map.set(keysArray[i], valuesArray[i]);
+    }
+  
+    return map;
+  }
+  
+  let keys = [1, 2, 3, 4];
+  let values = ["div", "span", "b", "i"];
+  let map = mapBuilder(keys, values);
+  
+  console.log(map.size); // 4
+  console.log(map.get(2)); // "span"
+
+  // 6
+
+  let arr = [];
+
+for (let i = 0; i <= 2; i++) {
+  (function (index) {
+    arr[index] = function () {
+      console.log(index);
+    };
+  })(i);
+}
+
+arr[0](); // 0
+arr[arr.length - 1](); // 2
+
   
